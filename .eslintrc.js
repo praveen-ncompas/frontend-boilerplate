cfba7abf4a28cd88208c8c0a54a2f9e7d36d4e5a
Praveen Pal <praveenpal4232@gmail.com>
2024-08-08 13:57:38 +0530

    Added checks

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/no-unknown-property': 0,
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false
+        }
+      }
+    ],
+    camelcase: 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
